6d33273c2e748605772a696be91b24959626c115
Tawana Kwaramba <tawanakwaramba@192-168-1-103.tpgi.com.au>
2021-09-09 17:14:35 +0800

    creating of the password table

--- app/src/main/java/com/example/assone/PasswordSchema.java
+++ app/src/main/java/com/example/assone/PasswordSchema.java
@@ -1,4 +1,19 @@
 package com.example.assone;
 
-public class PasswordSchema {
+public class PasswordSchema
+{
+    public static class PassWordTable
+    {
+        //the name of the table
+        public static final String Name = "passwords";
+
+        //creating the class of the table
+        public static class cols
+        {
+            //the name of each column in the created table
+            public static final String USER_NAME = "user_name";
+            public static final String PASSWORD = "user_password";
+        }
+
+    }
 }
